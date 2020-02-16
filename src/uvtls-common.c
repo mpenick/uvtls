7f9999ecdc0f4152f2e0b0fe804fe25e3d83d45b
Michael Penick <mpenick@users.noreply.github.com>
2020-02-16 15:33:35 -0500

    Restructure cmake build and start adding examples (#2)
    * Restructure cmake build and start adding examples
    * `uvtls_strerror_r()` and `uvtls_err_name_r()` not added until 1.22.0

--- src/uvtls-common.c
+++ src/uvtls-common.c
@@ -1,28 +1,38 @@
 /* Copyright Michael A. Penick
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is furnished to do
- * so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
  */
 
 #include "uvtls.h"
 
 #include <string.h>
 
+#define UVTLS_ERR_NAME_GEN(name, _) \
+  case UVTLS_##name:                \
+    return #name;
+const char* uvtls_err_name(int err) {
+  switch (err) { UVTLS_ERRNO_MAP(UVTLS_ERR_NAME_GEN) }
+  return uv_err_name(err);
+}
+#undef UVTLS_ERR_NAME_GEN
+
+#if UV_VERSION_HEX >= 0x011600 /* 1.22.0 */
 #define UVTLS_ERR_NAME_GEN_R(name, _) \
   case UVTLS_##name:                  \
     strncpy(buf, #name, buflen);      \
@@ -36,16 +46,18 @@ char* uvtls_err_name_r(int err, char* buf, size_t buflen) {
   return buf;
 }
 #undef UVTLS_ERR_NAME_GEN_R
+#endif
 
-#define UVTLS_ERR_NAME_GEN(name, _) \
-  case UVTLS_##name:                \
-    return #name;
-const char* uvtls_err_name(int err) {
-  switch (err) { UVTLS_ERRNO_MAP(UVTLS_ERR_NAME_GEN) }
-  return uv_err_name(err);
+#define UVTLS_STRERROR_GEN(name, msg) \
+  case UVTLS_##name:                  \
+    return msg;
+const char* uvtls_strerror(int err) {
+  switch (err) { UVTLS_ERRNO_MAP(UVTLS_STRERROR_GEN) }
+  return uv_strerror(err);
 }
-#undef UVTLS_ERR_NAME_GEN
+#undef UVTLS_STRERROR_GEN
 
+#if UV_VERSION_HEX >= 0x011600 /* 1.22.0 */
 #define UVTLS_STRERROR_GEN_R(name, msg) \
   case UVTLS_##name:                    \
     snprintf(buf, buflen, "%s", msg);   \
@@ -59,12 +71,4 @@ char* uvtls_strerror_r(int err, char* buf, size_t buflen) {
   return buf;
 }
 #undef UVTLS_STRERROR_GEN_R
-
-#define UVTLS_STRERROR_GEN(name, msg) \
-  case UVTLS_##name:                  \
-    return msg;
-const char* uvtls_strerror(int err) {
-  switch (err) { UVTLS_ERRNO_MAP(UVTLS_STRERROR_GEN) }
-  return uv_strerror(err);
-}
-#undef UVTLS_STRERROR_GEN
+#endif
